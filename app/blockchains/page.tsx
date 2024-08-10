4e3d386a8b198cf658a1f3c0a4235d5d537618c0
yyayourt <yanisnasri30@gmail.com>
2024-08-10 14:14:57 +0200

    Bitcoin page : first commit

--- app/blockchains/page.tsx
+++ app/blockchains/page.tsx
@@ -1,3 +1,82 @@
-export default function page() {
-  return <h1>About me</h1>;
+// frontend/src/pages/index.tsx
+
+import fs from 'fs';
+import path from 'path';
+import Image from 'next/image';
+import discord from '../../assets/Social/Discord.svg';
+import twitter from '../../assets/Social/twitter.svg';
+import { CarouselSize } from '../../components/Carousel/CarouselBooks';
+import { CarouselMedia } from '../../components/Carousel/CarouselMedia';
+import { CarouselCategoryX } from '../../components/Carousel/CarouselCategoryX';
+
+interface Article {
+  articleId: string;
+  title: string;
+  description: string;
+  webLink: string;
+  image: string;
+  category: string;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export default async function Page() {
+  const filePath = path.join(process.cwd(), 'article.json');
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  const articles: Article[] = JSON.parse(jsonData);
+  const articlesInCategoryX = articles.filter((article: Article) => article.category === "X");
+  const books = articles.filter((article: Article) => article.category === "Books");
+  const medias = articles.filter((article: Article) => article.category === "Medias");
+
+  return (
+    <>
+      <div className="z-10 w-full items-center p-[20px] justify-between font-mono text-sm lg:flex fixed bg-white">
+        <p className="text-gray-900 font-serif text-3xl font-medium normal-case not-italic no-underline leading-tight tracking-tighter">
+          Encyclochain
+        </p>
+        <p className="text-gray-900 font-serif text-3xl font-medium normal-case not-italic no-underline leading-tight tracking-tighter hidden lg:block">
+          Blockchains encyclopedia
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center gap-[30px] bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <a href="https://x.com/Encyclochain" target="_blank">
+            <div style={{ width: '20px', height: '24px', position: 'relative' }}>
+              <Image
+                src={twitter}
+                alt="Twitter Logo"
+                width={20}
+                height={24}
+                style={{ objectFit: 'contain' }}
+              />
+            </div>
+          </a>
+          <div style={{ width: '20px', height: '24px', position: 'relative' }}>
+            <Image
+              src={discord}
+              alt="Discord Logo"
+                width={20}
+                height={24}
+                style={{ objectFit: 'contain' }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#F7931A] flex h-[54vh] justify-center items-center flex-col">
+        <h1 className="text-white text-[104px] font-semibold">₿ITCOIN</h1>
+        <a href="https://bitcoin.org/bitcoin.pdf" className="bg-white text-[#F7931A] px-6 py-3 rounded-sm font-medium">Whitepaper</a>
+      </div>
+      <div className='flex justify-center items-center my-8 mb-[20vh]'>
+        <h2 className='font-bold text-xl'>Category X</h2>
+        <CarouselCategoryX articles={articlesInCategoryX} />
+      </div>
+      <div className='flex justify-center items-center my-8 mb-[20vh]'>
+        <h2 className='font-bold text-xl'>Books</h2>
+        <CarouselSize books={books} />
+      </div>
+      <div className='flex justify-center items-center my-8 mb-[20vh]'>
+        <h2 className='font-bold text-xl'>Medias</h2>
+        <CarouselMedia Medias={medias} />
+      </div>
+    </>
+  );
 }
---
+++ components/Carousel/CarouselBooks.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import Image from 'next/image';
+import { Card, CardContent } from "@/components/ui/card";
+import bookBit from '../../public/articles/early-bird.jpg';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+interface Article {
+  articleId: string;
+  title: string;
+  description: string;
+  webLink: string;
+  image: string;
+  category: string;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface CarouselSizeProps {
+  books: Article[];
+}
+
+export function CarouselSize({ books }: CarouselSizeProps) {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-[80%]"
+    >
+      <CarouselContent className="gap-[10%]">
+        {books.map((book) => (
+          <CarouselItem key={book.articleId} className="md:basis-1/2 lg:basis-[400px] h-[300px]">
+            <div className="w-[100%] h-[100%]">
+              <Card className="w-[100%] h-[100%] rounded-none">
+                <CardContent className="flex items-center justify-center w-[100%] h-[100%]">
+                  <a href={book.webLink} className=" w-[100%] h-[100%] flex justify-between flex-row gap-4">
+                    <div style={{ position: 'relative',width: '100%', height: '0', paddingBottom: '37.21%' }}>
+                      <Image src={bookBit} alt={book.title} layout="cover" width={645} height={240} style={{ objectFit: 'cover' }} className="w-[100%]"/>
+                    </div>
+                    <div className="w-[88%] pt-3 pr-3">
+                      <h3 className='font-semibold pb-4'>{book.title}</h3>
+                      <p>{book.description}</p>
+                    </div>
+                  </a>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
