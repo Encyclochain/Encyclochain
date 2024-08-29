53a230d35aa186a057ce3b448051ce050a8a4de3
Amine Sekkiou <amine1132@hotmail.fr>
2024-08-29 10:04:00 +0200

    Design (Blockchain page)

--- app/blockchains/page.tsx
+++ app/blockchains/page.tsx
@@ -1,11 +1,11 @@
-import prisma from '@/lib/db';
+import prisma from "@/lib/db";
 
-import { CarouselSize } from '../../components/Carousel/CarouselBooks';
-import { CarouselMedia } from '../../components/Carousel/CarouselMedia';
-import { CarouselCategoryX } from '../../components/Carousel/CarouselCategoryX';
+import { CarouselSize } from "../../components/Carousel/CarouselBooks";
+import { CarouselMedia } from "../../components/Carousel/CarouselMedia";
+import { CarouselCategoryX } from "../../components/Carousel/CarouselCategoryX";
 
-import { Header } from '@/components/Header';
-import { Banner } from '@/components/Blockchains/Banner';
+import { Header } from "@/components/Header";
+import { Banner } from "@/components/Blockchains/Banner";
 
 export default async function Page() {
   //requesting all categories corresponding to Bitcoin with all resources linked
@@ -28,27 +28,29 @@ export default async function Page() {
       blockchains: {
         some: {
           title: {
-            equals: 'Bitcoin',
+            equals: "Bitcoin",
           },
         },
       },
     },
-  })
+  });
 
   //TODO : real logic
   const isBitcoinPage = true;
 
   return (
     <main>
-      <Header design="z-10 w-full items-center p-[20px] justify-between font-mono text-sm lg:flex fixed bg-white"
+      <Header
+        design="z-10 w-full items-center p-[20px] justify-between font-mono text-sm lg:flex fixed bg-white"
         showArrow={isBitcoinPage} // Passez showArrow à true si vous êtes sur la page Bitcoin
       />
       <Banner />
-      {//creating one carousel for each category (TODO : do not show empty categories)
-        prisma_res.map((cat) => (
+      {
+        //creating one carousel for each category (TODO : do not show empty categories)
+        prisma_res.map((cat) =>
           cat.resources !== null ? (
             <div key={cat.id} className="flex items-center">
-              <div className="flex justify-center items-center my-8 mb-[12%] max-md:flex-col w-full">
+              <div className="flex justify-center items-center my-8 mb-[12%] max-md:flex-col w-full h-[500px]">
                 <h2 className="font-bold text-xl max-md:mb-[30px] mr-[20px]">
                   {cat.title}
                 </h2>
@@ -56,7 +58,8 @@ export default async function Page() {
               </div>
             </div>
           ) : null
-        ))}
-    </main >
+        )
+      }
+    </main>
   );
 }
--- components/LinkPreview.tsx
+++ components/LinkPreview.tsx
@@ -6,44 +6,45 @@ import { Button } from "./ui/button";
 
 //interface containing needed data
 export interface PreviewData {
-    title: string,
-    description: string,
-    image: string,
-    url: string,
-};
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+}
 
 //preview data is passed as a PreviewData object
 export function LinkPreview({ data }: { data: PreviewData }) {
+  //use default image if data is empty (NOT WORKING)
+  const imageSrc =
+    data.image === "nopic" ? "assets/articles/bitcoinmagazine.png" : data.image;
+  //console.log("Image source : ", imageSrc);
 
-    //use default image if data is empty (NOT WORKING)
-    const imageSrc = data.image === "nopic" ? "assets/articles/bitcoinmagazine.png" : data.image;
-    //console.log("Image source : ", imageSrc);
-
-    //design copied from CarouselCategoryX
-    return (
-        <div className="h-[100%]">
-            <Card className=" w-[100%] h-[100%] border-none">
-                <CardContent className="flex items-center justify-center w-[100%] h-[100%] md:h-[350px]">
-                    <a
-                        href={data.url}
-                        className=" w-[100%] flex flex-col gap-4"
-                    >
-                        <img
-                            src={imageSrc}
-                            alt={data.title}
-                            style={{ objectFit: 'cover' }}
-                            className="w-[100%]"
-                        />
-                        <div className="px-3 pb-3">
-                            <h3 className='font-semibold pb-2'>{data.title}</h3>
-                            <p>{data.description}</p>
-                        </div>
-                        <Button className="bg-[#F7931A] hover:bg-[#F7931A] rounded-none w-max p-[20px] max-md:w-full max-md:rounded-[5px] top-5">
-                            Explore
-                        </Button>
-                    </a>
-                </CardContent>
-            </Card>
-        </div>
-    );
+  //design copied from CarouselCategoryX
+  return (
+    <div className="h-[100%] w-[100%]">
+      <Card className=" w-[100%] h-[500px] border-none ">
+        <CardContent className="flex items-center justify-center w-[100%] h-[404px] md:h-[350px]">
+          <a
+            href={data.url}
+            className=" w-[100%] flex flex-col h-[500px] justify-between mt-[158px] max-md:mt-[90px] "
+            target="blank"
+          >
+            <img
+              src={imageSrc}
+              alt={data.title}
+              style={{ objectFit: "cover" }}
+              className="w-[100%] h-[226px]"
+            />
+            <div className="px-3 pb-3">
+              <h3 className="font-semibold pb-2">{data.title}</h3>
+              <p>{data.description}</p>
+            </div>
+            <Button className="bg-[#F7931A] hover:bg-[#F7931A] rounded-none w-max p-[20px] max-md:w-full max-md:rounded-[5px] top-5">
+              Explore
+            </Button>
+          </a>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
