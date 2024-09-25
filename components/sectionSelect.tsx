072530be265184651c14909f1fbea20889b4db37
amine1132 <amine1132@hotmail.fr>
2024-09-25 18:22:57 +0200

    design of the navbar , and design of the home page

--- components/sectionSelect.tsx
+++ components/sectionSelect.tsx
@@ -41,30 +41,30 @@ export async function getSectionsGroupedByType() {
         {sectionTypes.map((sectionType) => (
           // Vérifier si le sectionType a des sections associées avant de l'afficher
           sectionType.sections.length > 0 && (
-            <div key={sectionType.id} className="mb-[50px]">
+            <div key={sectionType.id} className="mb-[250px] flex flex-row">
               {/* Titre du type de section (ex: "Layer 1") */}
-              <h2 className="text-2xl font-bold text-gray-900 mb-6 lg:text-left text-center">
+              <h2 className="text-2xl font-bold text-gray-900 mb-6 lg:text-center text-center items-center justify-center flex">
                 {/* Utilisation de lg:text-left pour l'alignement à gauche sur grands écrans et text-center pour mobile */}
                 {sectionType.title} {/* Afficher le titre du sectionType */}
               </h2>
   
               {/* Carrousel pour les sections de ce type */}
-              <Carousel className="w-[80%] max-sm:w-[90%] mx-auto">
+              <Carousel  className="w-[80%] max-sm:w-[90%] mx-auto">
                 <CarouselContent className="flex gap-6">
                   {sectionType.sections.map((section) => (
-                    <CarouselItem key={section.id} className="flex flex-col items-center w-[30%]">
+                    <CarouselItem key={section.id} className="flex flex-col items-center w-[30%] basis-[30%] ">
                       {/* Image de la section */}
                       <img
                         src={section.sectionInfo?.imageLink || ""}
                         alt={`Logo ${section.title}`}
-                        className="dark:invert h-auto"
-                        width={185}
+                        className="dark:invert h-auto rounded-[50%]"
+                        width={150}
                         height={24}
                       />
   
                       {/* Bouton pour explorer la section */}
                       <Button
-                        className="mt-4 px-4 py-2 text-white rounded-md"
+                        className="mt-4 px-4 py-2 text-white rounded-md font-bold"
                         style={{
                           backgroundColor: section.sectionInfo?.color || "#000", // Couleur dynamique
                         }}
